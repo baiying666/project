fb016663de8a03d8c55be9a1b6df74a95b4e10ef
baiying <18519101291@163.com>
2017-06-19 00:12:05 +0800

    bookstore

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+import 'bootstrap/dist/css/bootstrap.css';
+//使用vueResource模块 ,使用后每个this上都有一个$http的属性
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+new Vue({
+  el: '#app',
+  router,
+  ...App
+});
